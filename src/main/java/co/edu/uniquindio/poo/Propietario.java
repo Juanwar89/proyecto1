44a2dfc278b1ba65a109c062adb73c201f96e796
jefreyramos <jefreyramos011@gmail.com>
2024-05-26 02:39:58 -0500

    ss

--- src/main/java/co/edu/uniquindio/poo/Propietario.java
+++ src/main/java/co/edu/uniquindio/poo/Propietario.java
@@ -1,25 +1,16 @@
 package co.edu.uniquindio.poo;
 
-public record Propietario(String nombre, String apellido, String iD)
-{
-  
-
- 
-  public String getNombre()
-  {
-    assert nombre != null && !nombre.isBlank() :"La placa no puede ser null y tampoco puede ser vacio";
-    return nombre;
-  }
-  public String getApellido()
-  {
-    assert apellido != null && !apellido.isBlank() :"El propietario no puede ser null y tampoco puede ser vacio";
-    return apellido;
-  }
-  public String getID()
-  {
-    assert iD != null && iD.isBlank() :"El modelo no puede ser null y tampoco puede ser vacio";
-    return iD;
-  }
-
-
+public record Propietario(String nombre, String apellido, String iD){
+    public String getNombre(){
+      assert nombre != null && !nombre.isBlank() :"La placa no puede ser null y tampoco puede ser vacio";
+      return nombre;
+    }
+    public String getApellido(){
+      assert apellido != null && !apellido.isBlank() :"El propietario no puede ser null y tampoco puede ser vacio";
+      return apellido;
+    }
+    public String getID(){
+      assert iD != null && iD.isBlank() :"El modelo no puede ser null y tampoco puede ser vacio";
+      return iD;
+    }
 }
